68036db6e0c75f29f534eecc07b051b315988309
zsJacky <zsCamden@gmail.com>
2018-08-12 14:36:53 +0800

    add chp4 example

---
+++ chp4/example/printout.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define PI 3.141593
+
+int main(void) {
+    int number = 5;
+    float expresso = 13.5;
+    int cost = 3100;
+    printf("The %d CEOS drank %f cups od expresso.\n", number, expresso);
+    printf("The value of pi is %f.\n", PI);
+    printf("Farewell! thou art too dear for my possessing, \n");
+    printf("%c%d\n", '$', 2 * cost);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
